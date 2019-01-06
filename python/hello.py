3335eb63e28c2020f47bff83dbb5b1d2bd5b0f0a
hsloss <hannahmaguiresloss@gmail.com>
2019-01-06 17:21:30 -0500

    replicated example case correctly.

--- python/hello.py
+++ python/hello.py
@@ -1,15 +1,20 @@
 mylist = [1,1,5,25,25,125,625]
 r = 5
+count = 0
 
 for i in range(len(mylist)):
-    for j in range(i, len(mylist)):
-        if mylist[i]*r == j:
-                continue
-        for k in range(j, len(mylist)):
-                if mylist[j]*r == k:
+    for j in range(i+1,len(mylist)):
+        if mylist[i]*r == mylist[j]:
+          print('not geometric')
+        else:  
+          continue        
+        for k in range(j+1,len(mylist)):
+                if mylist[i]*r**2 == mylist[k]:
                   print('begin')
                   print('index:', i, 'value:', mylist[i])
                   print('index:', j, 'value:', mylist[j])
                   print('index:', k, 'value:', mylist[k])
                   print('end')
+                  count += 1
+print('Total Indices: %d' %count)                  
                     
