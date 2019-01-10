8fe27689edd1865b50774a909dc1a1b293dd0763
hsloss <hannahmaguiresloss@gmail.com>
2019-01-10 12:32:30 -0500

    trying to make new component for changeName method.

--- vue/src/components/ChangeName.vue
+++ vue/src/components/ChangeName.vue
@@ -1,29 +1,12 @@
 <template>
-  <div>
-    <!-- justice leage application begins here -->
-    <h1 id="jl">Justice League Mission Planner</h1>
-
-    <ul class="roster">
-      <h3>Roster:</h3>
-      <li v-for="hero in heroes"
-          :key="hero.name">
-        <!-- to do: conditionally display this span -->
-        <span v-if="yes">✔ &nbsp;</span>
-        <span>{{ hero.name }}&nbsp;</span>
-        <span class="edit"
-              @click="editHero(hero)">edit</span>
-      </li>
-      <br>
-      <ChangeName :heroes="heroes" />
-      <br>
-      <span v-if="isEdit">enter to submit, click outside the box to cancel</span>
-    </ul>
-    <ChosenHeroes :heroes="heroes" />
-  </div>
+    <input type="text"
+            placeholder="new name"
+            v-model="newName"
+            v-if="isEdit"
+            @keyup.enter="$emit('changeName')"
+            @blur="clear">
 </template>
 
-
-<script>
 import ChosenHeroes from "./components/ChosenHeroes.vue";
 
 export default {
